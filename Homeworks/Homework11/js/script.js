085c191fcdbdca4685bf74e8ebfcb42f6198c0fe
VeronikaRed <rudanika26@gmail.com>
2020-09-22 15:04:29 +0300

    JS intro Homework

---
+++ Homeworks/Homework11/js/script.js
@@ -0,0 +1,27 @@
+let a = 6; b = 3;
+let result = (a > b) ? true : false;
+console.log(result);
+
+a = 3; b = 3;
+result = (a == b);
+console.log(result);
+
+a = 3; b = 6;
+if (a!=b) {
+    result = true;
+}else{
+    result = false;
+};
+console.log(result);
+
+a = 1; b = "";
+console.log(a===b);
+
+console.log("B">"A");
+
+console.log("a">"Z");
+
+a = false; b = 0;
+console.log(a >= b);
+
+console.log(("2" === 2) ? "ok" : "not")
