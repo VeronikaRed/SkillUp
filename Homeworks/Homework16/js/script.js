f83aae168f543b3714b025a567ad071a8c9eefca
VeronikaRed <rudanika26@gmail.com>
2020-10-05 09:45:59 +0300

    refactor homework 16

--- Homeworks/Homework16/js/script.js
+++ Homeworks/Homework16/js/script.js
@@ -190,7 +190,7 @@ console.log(arr);
 // 18. Таблица умножения в цикле
 for (let i=1; i<=9; i++) {
     for (let j=1; j<=9; j++) {
-        // console.log(i + ' x ' + j + ' = ' + i*j);
+        console.log(i + ' x ' + j + ' = ' + i*j);
     }
 }
 
