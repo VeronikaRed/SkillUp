90e29352828c25c28562090434e1858fecd2c630
VeronikaRed <rudanika26@gmail.com>
2020-10-18 12:25:42 +0300

    refactoring

--- Homeworks/Homework13/js/script.js
+++ Homeworks/Homework13/js/script.js
@@ -1,45 +1,44 @@
-// функция принимает пределы случайного числа (min, max) и вывести в
-// консоль четное или нечетное оно
+/*the function takes the limits of a random number (min, max) and output to the 
+console is odd or even*/
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function checkNumber(num) {
     if (num % 2 == 1) {
-        return num + " - is odd number"
+        return num + " - is odd number";
     } else {
-        return num + " - is even number"
+        return num + " - is even number";
     }
 }
+// call function checkNumber for checking odd number.
+console.log(checkNumber(getRandomInt(1, 100)));
 
-console.log(checkNumber(getRandomInt(1, 100)))
 
-
-// есть строка "lorem ipsum is simply dummy", сделать новую строку, чтобы
-// первое слово было с большой буквы
+/* there is a string "lorem ipsum is simply dummy", make a new string with first
+capitalized letter */
 let str = "lorem ipsum is simply dummy";
-let str1 = str[0].toUpperCase() + str.slice(1, str.length)
-console.log(str1)
-
+let str1 = str[0].toUpperCase() + str.slice(1, str.length);
+console.log(str1);
 
 let str3 = str[0].toUpperCase() + "";
 for (i=1; i<str.length; i++){
-    str3 += str[i]
+    str3 += str[i];
 }
-console.log(str3)
+console.log(str3);
 
-
-// функция принимает две строки и возвращает большую из них
+/* the function takes two strings and returns the largest one */
+function checkLength(str1, str2){
+    if(str1.length > str2.length){
+        return str1;
+    }else if(str1.length < str2.length){
+        return str2;
+    }else{
+        return "I can't choose";
+    }
+}
 let string1 = "lorem";
 let string2 = "is";
 let length = 0;
-function checkLength(str1, str2){
-    if(str1.length > str2.length){
-        return str1
-    }else if(str1.length < str2.length){
-        return str2
-    }else{
-        return "I can't choose"
-    }
-}
+// call function checkLength for checking greater string length.
 console.log(checkLength(string1, string2));
