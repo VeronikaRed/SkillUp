2f11d8664e727d095ba511fa5d663f925c929c77
VeronikaRed <rudanika26@gmail.com>
2020-10-19 20:01:58 +0300

    add flexability

--- Homeworks/Homework19/js/script.js
+++ Homeworks/Homework19/js/script.js
@@ -1,12 +1,12 @@
-async function getGitPhoto() {
-    let url = "https://api.github.com/users/VeronikaRed";
+async function getGitPhoto(userName) {
+    let url = `https://api.github.com/users/${userName}`;
     let response = await fetch(url);
     let content = await response.json();
     let img = document.createElement("img");
     img.setAttribute("src", content.avatar_url);
     document.body.prepend(img);
 }
-getGitPhoto();
+getGitPhoto("VeronikaRed");
 
 
 async function getFakeJson(target) {
