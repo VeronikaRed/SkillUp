b8d55583a5f80f8211d140a2c14f56b6d824cebe
VeronikaRed <rudanika26@gmail.com>
2020-11-17 12:12:17 +0200

    add prettier

--- Homeworks/Homework21/js/script.js
+++ Homeworks/Homework21/js/script.js
@@ -1,35 +1,35 @@
 class Timer {
-    constructor(){
+    constructor() {
         this.time = 0;
         this.interval = null;
     }
-    start(){
-        this.interval = setInterval(function(){
+    start() {
+        this.interval = setInterval(function () {
             document.getElementById('div').innerHTML = timer.time++;
-            },1000);
-    };
-    stop(){
+        }, 1000);
+    }
+    stop() {
         clearInterval(this.interval);
         this.interval = null;
-    };
-    reset(){
+    }
+    reset() {
         this.time = 0;
         document.getElementById('div').innerHTML = timer.time;
-    };
+    }
 }
 
 let timer = new Timer();
 
-document.getElementById('stop').addEventListener('click', () =>{
-timer.stop();
+document.getElementById('stop').addEventListener('click', () => {
+    timer.stop();
 });
 
-document.getElementById('reset').addEventListener('click', () =>{
+document.getElementById('reset').addEventListener('click', () => {
     timer.reset();
 });
 
-document.getElementById('start').addEventListener('click', () =>{
-    if(timer.interval == null){
+document.getElementById('start').addEventListener('click', () => {
+    if (timer.interval == null) {
         timer.start();
-    } 
+    }
 });
