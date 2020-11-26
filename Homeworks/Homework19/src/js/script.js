6fc71e930f5dd79bcd412406bdd15e7cbb3943ba
VeronikaRed <rudanika26@gmail.com>
2020-11-26 15:27:45 +0200

    add coment

--- Homeworks/Homework19/src/js/script.js
+++ Homeworks/Homework19/src/js/script.js
@@ -2,6 +2,11 @@ import { getGitPhoto } from '../components/getGitPhoto';
 import { getFakeJson } from '../components/getFakeJson';
 import { getWeather } from '../components/getWeather';
 
+// task1
 getGitPhoto('VeronikaRed');
+
+// task2
 getFakeJson('posts');
+
+// task3
 getWeather();
